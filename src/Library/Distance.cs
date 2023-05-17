1c2d38492d5854948754793351fc61b28b74784f
Ramirods <ramiro.dasilva@correo.ucu.edu.uy>
2023-05-17 08:20:11 -0300

    upload2

---
+++ src/Library/Distance.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace proyect;
+
+public class Distance
+{
+    public static double CalculateDistance(RefugeLocation refugeLocation, UserLocation userLocation)
+    {
+        //Lógica para calcular la distancia
+    }
+
+}
---
+++ src/Library/Location.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace proyect;
+
+public class Location
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+}
